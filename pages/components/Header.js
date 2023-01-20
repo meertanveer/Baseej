f334a11196fad7f92d6d7bd94da208745763b70c
tanveerhydari313@gmail.com <tanveerhydari313@gmail.com>
2023-01-20 16:57:36 +0530

    header modified

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,10 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
+import ico from '../../public/images/ico.jpg';
 
 function Header() {
   return (
-    <div className="bg-slate-900 h-10 flex items-center justify-center">
+    <div className="bg-slate-900 h-10 gap-24 flex items-center pl-6">
+      <div>
+        <Image
+          src={ico}
+          alt="ico"
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+      </div>
       <div className="text-green-400">
-        <h2 className="text-xl font-bold font-serif">Baseej</h2>
+        <h2 className="text-2xl font-bold font-serif">Baseej</h2>
       </div>
     </div>
   );
--- pages/screens/Register.js
+++ pages/screens/Register.js
@@ -7,6 +7,9 @@ function Register() {
   return (
     <>
       <Header />
+      <div className="flex text-orange-500 justify-center font-bold font-mono text-2xl mt-10">
+        <p>Welcome</p>
+      </div>
       <div className="grid text-orange-500 items-center justify-center">
         <div className="grid grid-cols-1 w-64 rounded-lg space-y-2 mt-16 mb-32 p-6 border  text-medium text-sm border-slate-300 shadow-md shadow-orange-500">
           <label>Username</label>
@@ -27,6 +30,15 @@ function Register() {
             className=" rounded-sm bg-yellow-100 pl-4"
             placeholder="Confirm Password "
           ></input>
+          <div className="grid grid-cols-1 gap-2">
+            <label>Gender</label>
+            <div className="flex gap-4">
+              <input type="radio" id="ismale"></input>
+              <label for="male">Male</label>
+              <input type="radio" id="isfemale"></input>
+              <label for="female">Female</label>
+            </div>
+          </div>
           <button
             type="submit"
             className="text-green-500 border border-green-500 px-2 rounded-md hover:bg-green-500 hover:text-white"
